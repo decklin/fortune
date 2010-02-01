228d8de6c3f0fd9e8d1209a7540489c1f1736757
Decklin Foster <decklin@red-bean.com>
2010-02-01 12:12:02 -0500

    Make background fixed

--- ntp.js
+++ ntp.js
@@ -5,8 +5,9 @@ function randomFortune(text) {
 }
 
 function init() {
-    document.body.style.background = '-webkit-gradient(linear,0 0,0 100%,' +
-        'from(' + config.get('backgroundFrom') + '),' +
+    document.body.style.background = 'fixed ' +
+        '-webkit-gradient(linear, 0 0, 0 100%,' +
+        'from(' + config.get('backgroundFrom') + '), ' +
         'to(' + config.get('backgroundTo') + '))';
 
     var fortune = document.getElementById('fortune');
